golvok/train-scheduler

commit 0b8e2c4210283ab1f42cbb0180e37a829096dcd5

add debug levels - all are enabled for now.

util/logging.h++
@@ -4,22 +4,113 @@
 
 #include <util/utils.h++>
 
+#include <bitset>
 #include <boost/iostreams/filtering_stream.hpp>
+#include <cstdint>
 #include <iostream>
 #include <sstream>
-#include <cstdint>
 #include <string>
 
-class IndentingDebugPrinter;
+class IndentingLeveledDebugPrinter;
+
+class DebugLevel {
+public:
+	enum Level {
+		INFO,
+		WARN,
+		ERROR,
+
+		WC_D1, // Wanted capacity debug
+		WC_D2, // Wanted capacity debug level 2
+		WC_D3, // Wanted capacity lowest level debug
+		TR_D1, // Train Roting debug
+		TR_D2, // Train Roting debug level 2
+		TR_D3, // Train Roting lowest level debug
+		PR_D1, // Passenger Roting debug
+		PR_D2, // Passenger Roting debug level 2
+		PR_D3, // Passenger Roting lowest level debug
+		LEVEL_COUNT,
+	};
+};
+
+using DL = DebugLevel::Level;
+
+template<
+	typename STREAM_TYPE,
+	typename REDIRECT_TYPE,
+	typename LEVEL_TYPE,
+	size_t NUM_LEVELS
+>
+class LevelRedirecter {
+private:
+	std::bitset<NUM_LEVELS> enabled_levels;
+
+	STREAM_TYPE* cast_to_str() { return static_cast<STREAM_TYPE*>(this); }
+public:
+	LevelRedirecter()
+		: enabled_levels()
+	{ }
+
+	virtual ~LevelRedirecter() = default;
+
+	LevelRedirecter(const LevelRedirecter&) = delete;
+	LevelRedirecter& operator=(const LevelRedirecter&) = delete;
+
+	LevelRedirecter(LevelRedirecter&&) = default;
+	LevelRedirecter& operator=(LevelRedirecter&&) = default;
+
+	REDIRECT_TYPE operator()(const LEVEL_TYPE& level) {
+		if (enabled_levels.test(level)) {
+			return REDIRECT_TYPE(cast_to_str());
+		} else {
+			return REDIRECT_TYPE(nullptr);
+		}
+	}
+
+	template<typename T>
+	void enable_level(const T& level) {
+		set_enable_for_level(level,true);
+	}
+
+	template<typename T>
+	void disable_level(const T& level) {
+		set_enable_for_level(level,false);
+	}
+
+	template<typename LOCAL_LEVEL_TYPE>
+	void set_enable_for_level(
+		const LOCAL_LEVEL_TYPE& level,
+		bool enable,
+		typename EnableIfEnum<LOCAL_LEVEL_TYPE>::type** = 0,
+		typename std::enable_if<std::is_same<LOCAL_LEVEL_TYPE,LEVEL_TYPE>::value>::type* = 0
+	) {
+		enabled_levels.set(
+			static_cast<std::underlying_type_t<LOCAL_LEVEL_TYPE>>(level),
+			enable
+		);
+	}
+
+	template<typename LOCAL_LEVEL_TYPE>
+	void set_enable_for_level(
+		const LOCAL_LEVEL_TYPE& level,
+		bool enable,
+		typename EnableIfIntegral<LOCAL_LEVEL_TYPE>::type* = 0
+	) {
+		enabled_levels.set(
+			level,
+			enable
+		);
+	}
+};
 
 class indent_filter	: public boost::iostreams::output_filter {
 private:
-	friend class IndentingDebugPrinter;
+	friend class IndentingLeveledDebugPrinter;
 
-	IndentingDebugPrinter& src;
+	IndentingLeveledDebugPrinter& src;
 	bool just_saw_newline;
 
-	explicit indent_filter(IndentingDebugPrinter& src)
+	explicit indent_filter(IndentingLeveledDebugPrinter& src)
 		: src(src)
 		, just_saw_newline(true)
 	{ }
@@ -32,10 +123,11 @@ public:
 };
 
 class IndentLevel {
-	friend class IndentingDebugPrinter;
-	IndentingDebugPrinter* src;
+	friend class IndentingLeveledDebugPrinter;
+	friend class LevelStream;
+	IndentingLeveledDebugPrinter* src;
 	bool ended;
-	IndentLevel(IndentingDebugPrinter* src) : src(src), ended(false) { }
+	IndentLevel(IndentingLeveledDebugPrinter* src) : src(src), ended(false) { }
 public:
 	void endIndent();
 	~IndentLevel();
@@ -59,15 +151,51 @@ public:
 	IndentLevel& operator=(const IndentLevel&) = delete;
 };
 
-class IndentingDebugPrinter : boost::iostreams::filtering_ostream {
-	friend class IndentLevel;
+class LevelStream {
+private:
+	friend class IndentingLeveledDebugPrinter;
+
+	IndentingLeveledDebugPrinter* src;
+
+public:
+	LevelStream(IndentingLeveledDebugPrinter* src)
+		: src(src)
+	{ }
+
+	LevelStream(const LevelStream&) = default;
+	LevelStream(LevelStream&&) = default;
+
+	LevelStream& operator=(const LevelStream&) = default;
+	LevelStream& operator=(LevelStream&&) = default;
+
+	template<typename T>
+	friend LevelStream& operator<<(LevelStream& lhs, const T& t);
+
+	template<typename T>
+	IndentLevel indentWithTitle(const T& t);
+
+	bool enabled() { return src != nullptr; }
+};
+
+class IndentingLeveledDebugPrinter
+	: private boost::iostreams::filtering_ostream
+	, public LevelRedirecter
+		< IndentingLeveledDebugPrinter
+		, LevelStream
+		, DebugLevel::Level
+		, DebugLevel::LEVEL_COUNT
+	>
+{
 	int max_indent_level;
 	int indent_level;
 
 	std::stringstream ss;
 public:
-	IndentingDebugPrinter(std::ostream& os, int max_indent_level)
-		: max_indent_level(max_indent_level)
+
+	IndentingLeveledDebugPrinter(std::ostream& os, int max_indent_level)
+		: boost::iostreams::filtering_ostream()
+		, LevelRedirecter()
+		, max_indent_level(max_indent_level)
 		, indent_level(0)
 		, ss()
 	{
@@ -75,8 +203,8 @@ public:
 		push(os);
 	}
 
-	IndentingDebugPrinter(const IndentingDebugPrinter&) = delete;
-	IndentingDebugPrinter& operator=(const IndentingDebugPrinter&) = delete;
+	IndentingLeveledDebugPrinter(const IndentingLeveledDebugPrinter&) = delete;
+	IndentingLeveledDebugPrinter& operator=(const IndentingLeveledDebugPrinter&) = delete;
 
 	template<typename T>
 	void print(const T& t) {
@@ -107,19 +235,24 @@ public:
 	}
 
 	template<typename FUNC>
-	auto indentWithTitle(const FUNC& f) -> decltype(f(*this),IndentLevel(this)) {
+	auto indentWithTitle(const FUNC& f) -> decltype(f(ss),IndentLevel(this)) {
 		// the weird return value is so the compiler SFINAE's away this
 		// overload if FUNC is not a lambda style type
 		util::repeat(getTitleLevel(),[&](){
-			(*this) << '=';
+			print('=');
 		});
-		(*this) << ' ';
-		f(*this);
-		(*this) << ' ';
+		print(' ');
+
+		f(ss);
+		std::string title = ss.str();
+		print(title);
+		std::stringstream().swap(ss); // empty the stringstream
+
+		print(' ');
 		util::repeat(getTitleLevel(),[&](){
-			(*this) << '=';
+			print('=');
 		});
-		(*this) << '\n';
+		print('\n');
 		indent_level++;
 		return IndentLevel(this);
 	}
@@ -134,6 +267,7 @@ public:
 
 	void setMaxIndentation(int level) { max_indent_level = level; }
 private:
+	friend class IndentLevel;
 	void endIndent() {
 		if (indent_level > 0) {
 			indent_level = indent_level - 1;
@@ -141,12 +275,28 @@ private:
 	}
 };
 
+extern IndentingLeveledDebugPrinter dout;
+
 template<typename T>
-IndentingDebugPrinter& operator<<(IndentingDebugPrinter& lhs, const T& rhs) {
-	lhs.print(rhs);
+LevelStream& operator<<(LevelStream&& lhs, const T& t) {
+	return lhs << t;
+}
+
+template<typename T>
+LevelStream& operator<<(LevelStream& lhs, const T& t) {
+	if (lhs.enabled()) {
+		lhs.src->print(t);
+	}
 	return lhs;
 }
 
-extern IndentingDebugPrinter dout;
+template<typename T>
+IndentLevel LevelStream::indentWithTitle(const T& t) {
+	if (enabled()) {
+		return src->indentWithTitle(t);
+	} else {
+		return IndentLevel(nullptr);
+	}
+}
 
 #endif /* UTIL__LOGGING_H */

algo/scheduler.c++
@@ -72,11 +72,13 @@ public:
 	);
 
 	void dump_edge_wanted_capacites_to_dout(
-		const EdgeWantedCapacities& edge_wanted_capacities
+		const EdgeWantedCapacities& edge_wanted_capacities,
+		DebugLevel::Level level
 	);
 
 	void dump_trains_to_dout(
-		const std::vector<Route>& trains
+		const std::vector<Route>& trains,
+		DebugLevel::Level level
 	);
 };
 
@@ -96,7 +98,7 @@ Schedule schedule(
 }
 
 Schedule Scheduler::do_schedule() {
-	auto fnd_routes_indent = dout.indentWithTitle("finding routes (scheduler2)");
+	auto fnd_routes_indent = dout(DL::INFO).indentWithTitle("finding routes (scheduler2)");
 
 	auto edge_wanted_capacities_sp = util::make_shared(::util::makeEdgeMap<float>(g,float()));
 	auto& edge_wanted_capacities = *edge_wanted_capacities_sp;
@@ -120,12 +122,12 @@ Schedule Scheduler::do_schedule() {
 Scheduler::EdgeWantedCapacities Scheduler::compute_edge_wanted_capacities() {
 	auto edge_wanted_capacities = ::util::makeEdgeMap<float>(g,1);
 
-	auto ewc_indent = dout.indentWithTitle("Computing Edge Wanted Capacities");
+	auto ewc_indent = dout(DL::WC_D1).indentWithTitle("Computing Edge Wanted Capacities");
 	for (const Passenger& p : passengers) {
-		auto p_indent = dout.indentWithTitle([&](auto&& out){ out << "Passenger " << p.getName(); });
+		auto p_indent = dout(DL::WC_D1).indentWithTitle([&](auto&& out){ out << "Passenger " << p.getName(); });
 		auto next_iteration_weights = network.makeEdgeWeightMapCopy();
 		for (uint iteration_num = 1; true; ++iteration_num) {
-			auto p_indent = dout.indentWithTitle([&](auto&& out){ out << "Iteration " << iteration_num; });
+			auto p_indent = dout(DL::WC_D2).indentWithTitle([&](auto&& out){ out << "Iteration " << iteration_num; });
 			auto iteration_weights = next_iteration_weights; // get ourselves a copy
 
 			auto iteration_weights_f = [&](TrackNetwork::BackingGraphType::edge_descriptor edge_desc) {
@@ -137,7 +139,7 @@ Scheduler::EdgeWantedCapacities Scheduler::compute_edge_wanted_capacities() {
 				network,
 				weight_map
 			);
-			::util::print_route(route_for_p,network,dout);
+			::util::print_route(route_for_p,network,dout(DL::WC_D2));
 			{
 				auto prev_vertex = route_for_p[0];
 				bool first = true;
@@ -159,18 +161,19 @@ Scheduler::EdgeWantedCapacities Scheduler::compute_edge_wanted_capacities() {
 		}
 	}
 
-	dump_edge_wanted_capacites_to_dout(edge_wanted_capacities);
+	dump_edge_wanted_capacites_to_dout(edge_wanted_capacities,DL::WC_D1);
 
 	return edge_wanted_capacities;
 }
 
 void Scheduler::dump_edge_wanted_capacites_to_dout(
-	const EdgeWantedCapacities& edge_wanted_capacities
+	const EdgeWantedCapacities& edge_wanted_capacities,
+	DebugLevel::Level level
 ) {
-	auto edge_wc_indent = dout.indentWithTitle("Computed edge wanted capacities");
+	auto edge_wc_indent = dout(level).indentWithTitle("Computed edge wanted capacities");
 	for (auto edge_desc : make_iterable(edges(g))) {
 		auto edge_index = network.getEdgeIndex(edge_desc);
-		dout << edge_index << " : " << edge_wanted_capacities[edge_index] << '\n';
+		dout(level) << edge_index << " : " << edge_wanted_capacities[edge_index] << '\n';
 	}
 }
 
@@ -178,7 +181,7 @@ void Scheduler::dump_edge_wanted_capacites_to_dout(
 std::vector<Scheduler::Route> Scheduler::make_rotues(
 	const EdgeWantedCapacities& edge_wanted_capacities
 ) {
-	auto routing_indent = dout.indentWithTitle("making train routes");
+	auto routing_indent = dout(DL::INFO).indentWithTitle("making train routes");
 
 
 	auto trains_go_to_vertex = ::util::makeVertexMap<bool>(g,false);
@@ -186,7 +189,7 @@ std::vector<Scheduler::Route> Scheduler::make_rotues(
 
 	std::vector<Route> routes;
 
-	auto routing_indent2 = dout.indentWithTitle("Traversing");
+	auto routing_indent2 = dout(DL::TR_D1).indentWithTitle("Traversing");
 
 	while (true) {
 
@@ -211,7 +214,7 @@ std::vector<Scheduler::Route> Scheduler::make_rotues(
 
 	routing_indent2.endIndent();
 
-	dump_trains_to_dout(routes);
+	dump_trains_to_dout(routes,DL::TR_D1);
 
 	return routes;
 }
@@ -221,7 +224,7 @@ std::tuple<Scheduler::Route, Scheduler::TrainsGoToVertex, uint> Scheduler::make_
 	TrainsGoToVertex&& trains_go_to_vertex,
 	uint vertex_covered_count
 ) {
-	auto train_indent = dout.indentWithTitle("Train");
+	auto train_indent = dout(DL::TR_D1).indentWithTitle("Train");
 
 
 	Route route;
@@ -237,7 +240,7 @@ std::tuple<Scheduler::Route, Scheduler::TrainsGoToVertex, uint> Scheduler::make_
 		);
 
 		if (next == TrackNetwork::ID(-1)) {
-			dout << "didn't find anything. Ending route.\n";
+			dout(DL::TR_D3) << "didn't find anything. Ending route.\n";
 			break;
 		} else {
 			if (trains_go_to_vertex[next] == false) {
@@ -260,7 +263,7 @@ TrackNetwork::ID Scheduler::compute_next_vertex(
 	const TrainsGoToVertex& trains_go_to_vertex,
 	TrackNetwork::ID curr
 ) {
-	auto vertex_index = dout.indentWithTitle([&](auto&&out){ out << "Vertex " << network.getVertexName(curr); });
+	auto vertex_index = dout(DL::TR_D2).indentWithTitle([&](auto&&out){ out << "Vertex " << network.getVertexName(curr); });
 
 	// check if all adjacent vertices already have trains -- cache?
 	bool heed_trains_going_to = false;
@@ -272,7 +275,7 @@ TrackNetwork::ID Scheduler::compute_next_vertex(
 	}
 
 	if (!heed_trains_going_to) {
-		dout << "-- ignoring existing trains --\n";
+		dout(DL::TR_D3) << "-- ignoring existing trains --\n";
 	}
 
 	TrackNetwork::ID next(-1);
@@ -280,17 +283,17 @@ TrackNetwork::ID Scheduler::compute_next_vertex(
 
 	for (auto e_desc : make_iterable(out_edges(curr,g))) {
 		TrackNetwork::ID target_v = target(e_desc,g);
-		dout << "looking at " << network.getVertexName(target_v) << ", ";
+		dout(DL::TR_D3) << "looking at " << network.getVertexName(target_v) << ", ";
 
 		if (heed_trains_going_to && trains_go_to_vertex[target_v]) {
-			dout << "skipping!\n";
+			dout(DL::TR_D3) << "skipping!\n";
 			continue;
 		}
 
 		float wc = edge_wanted_capacities[network.getEdgeIndex(e_desc)];
-		dout << "wc = " << wc;
+		dout(DL::TR_D3) << "wc = " << wc;
 		if (wc > best_wanted_capacity) {
-			dout << " - is best!\n";
+			dout(DL::TR_D3) << " - is best!\n";
 			next = target_v;
 			best_wanted_capacity = wc;
 		}
@@ -300,13 +303,14 @@ TrackNetwork::ID Scheduler::compute_next_vertex(
 }
 
 void Scheduler::dump_trains_to_dout(
-	const std::vector<Route>& trains
+	const std::vector<Route>& trains,
+	DebugLevel::Level level
 ) {
-	auto output_indent = dout.indentWithTitle("Trains");
+	auto output_indent = dout(level).indentWithTitle("Trains");
 	uint i = 1;
 	for (auto& route : trains) {
-		auto route_indent = dout.indentWithTitle([&](auto&&out){ out << "train " << i; });
-		::util::print_route(route,network,dout);
+		auto route_indent = dout(level).indentWithTitle([&](auto&&out){ out << "train " << i; });
+		::util::print_route(route,network,dout(level));
 		++i;
 	}
 }

main.c++
@@ -25,6 +25,10 @@ int main(int argc_int, char const** argv) {
 		graphics::get().initialize();
 	}
 
+	for (std::underlying_type_t<DebugLevel::Level> i = 0; i < DL::LEVEL_COUNT; ++i) {
+		dout.enable_level(static_cast<DebugLevel::Level>(i));
+	}
+
 	return program_main();
 }
 
@@ -48,9 +52,9 @@ int program_main() {
 			break;
 		}
 
-		dout << '\n';
-		auto d_indent = dout.indentWithTitle([&](auto&& s){s << "Input Data #" << tn_counter;});
-		dout << '\n';
+		dout(DL::INFO) << '\n';
+		auto d_indent = dout(DL::INFO).indentWithTitle([&](auto&& s){s << "Input Data #" << tn_counter;});
+		dout(DL::INFO) << '\n';
 
 		std::shared_ptr<algo::Schedule> schedule = std::make_shared<algo::Schedule>();
 		(*schedule) = algo::schedule(*tn, *passengers);

util/logging.c++
@@ -28,4 +28,4 @@ IndentLevel::~IndentLevel() {
 	}
 }
 
-IndentingDebugPrinter dout(std::cout, 0);
+IndentingLeveledDebugPrinter dout(std::cout, 0);

util/routing_utils.h++
@@ -42,7 +42,7 @@ std::unordered_map<Passenger,typename std::vector<TrackNetwork::ID>> get_shortes
 	std::unordered_map<Passenger,typename std::vector<TrackNetwork::ID>> passenger2route;
 
 	for (auto passenger : passengers) {
-		dout << "shortest path for " << passenger.getName() << " (enters at time " << passenger.getStartTime() << "):\n";
+		dout(DL::INFO) << "shortest path for " << passenger.getName() << " (enters at time " << passenger.getStartTime() << "):\n";
 
 		auto route = get_shortest_route(passenger.getEntryId(),passenger.getExitId(),network);
 
@@ -56,7 +56,7 @@ template<typename CONTAINER, typename OSTREAM>
 void print_route(
 	const CONTAINER& route,
 	const TrackNetwork& network,
-	OSTREAM& os
+	OSTREAM&& os
 ) {
 	std::for_each(std::begin(route), std::end(route), [&](auto& v){
 		os << network.getVertexName(v) << " -> ";

util/utils.h++
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <memory>
+#include <type_traits>
 
 template <typename...> struct all_convertible;
 
@@ -97,5 +98,11 @@ template<typename T, typename U>
 ComparesWithTag<T,U> compare_with_tag(const T& thing, U id) {
 	return ComparesWithTag<T,U>(thing,id);
 }
+
+template<typename T, typename V = void>
+using EnableIfEnum = typename std::enable_if<std::is_enum<T>::value,V>;
+
+template<typename T, typename V = void>
+using EnableIfIntegral = typename std::enable_if<std::is_integral<T>::value,V>;
 
 #endif /* UTIL_H */

commit 60b056a83076679e0f8f0aad2952da31bb2fab95

fix duplication and indentation problem in output introduced by 0b8e2c42

util/logging.h++
@@ -318,7 +318,7 @@ private:
 	friend class LevelStream;
 
 	template<typename FUNC>
-	auto indentWithTitle(const FUNC& f) -> decltype(f(ss),IndentLevel(this)) {
+	auto indentWithTitle(const FUNC& f) -> decltype(f(std::stringstream()),IndentLevel(this)) {
 		// the weird return value is so the compiler SFINAE's away this
 		// overload if FUNC is not a lambda style type
 		util::repeat(getTitleLevel(),[&](){
@@ -326,10 +326,10 @@ private:
 		});
 		print(' ');
 
-		f(ss);
-		std::string title = ss.str();
+		std::stringstream local_ss;
+		f(local_ss);
+		std::string title = local_ss.str();
 		print(title);
-		std::stringstream().swap(ss); // empty the stringstream
 
 		print(' ');
 		util::repeat(getTitleLevel(),[&](){